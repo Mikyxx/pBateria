e49504e04a8b24ec28b144e5baa595ce442474d1
Miqueias Barros <103081596+Mikyxx@users.noreply.github.com>
2025-04-24 20:10:29 -0300

    Project 1.1

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ document.body.addEventListener('keyup', (event)=>{
     playSound(event.code.toLowerCase());
 });
 
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+
+    if(song !== '') {
+        let songArray = song.split('');
+        console.log(songArray);
+    }
+})
+
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
     if(audioElement) {
+        audioElement.currentTime = 0;
         audioElement.play();
     }
 
     if(keyElement) {
         keyElement.classList.add('active');
+
+        setTimeout(()=>{
+            keyElement.classList.remove('active');
+        }, 300);
     }
 }
